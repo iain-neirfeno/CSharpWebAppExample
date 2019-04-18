91ab28253615411db853a320d6de0154b129910d
Iain Mackay <me@neirfeno.com>
2019-04-18 19:57:07 +0100

    [add] implementation of the triangle repository

---
+++ CalculatedTriangleRepo/TriangleRepo.cs
@@ -0,0 +1,13 @@
+using Triangles.Model;
+using Triangles.TriangleByPosition;
+
+namespace CalculatedTriangleRepo
+{
+    public class TriangleRepo: ITriangleByPositionRepo
+    {
+        public Triangle FindTriangleByRowAndColumn(char row, int column)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CalculatedTriangleRepoTest/TriangleRepoTest.cs
@@ -0,0 +1,14 @@
+using CalculatedTriangleRepo;
+
+namespace CalculatedTriangleRepoTest
+{
+    public class TriangleRepoTest
+    {
+        private readonly TriangleRepo _testSubject;
+
+        public TriangleRepoTest()
+        {
+            _testSubject = new TriangleRepo();
+        }
+    }
+}
