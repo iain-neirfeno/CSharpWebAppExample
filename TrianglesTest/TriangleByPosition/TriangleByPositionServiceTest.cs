07e7309c8075189cde715c1bd79eaad3204b1639
Iain Mackay <me@neirfeno.com>
2019-04-18 19:49:30 +0100

    [add] tests to cover edge cases

--- TrianglesTest/TriangleByPosition/TriangleByPositionServiceTest.cs
+++ TrianglesTest/TriangleByPosition/TriangleByPositionServiceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Moq;
 using Triangles.Model;
 using Triangles.TriangleByPosition;
@@ -29,6 +30,50 @@ namespace TrianglesTest.TriangleByPosition
             );
             var result = _testSubject.GetTriangleByPosition('A', 1);
             Assert.IsType<TriangleVerticesDto>(result);
+        }
+        
+        [Fact]
+        public void TestGetTriangleCoordinatesThrowsExceptionIfNotExists()
+        {
+            _repo.Setup(_ => _.FindTriangleByRowAndColumn(It.IsAny<char>(), It.IsAny<int>())).Returns((Triangle) null);
+            Assert.Throws<ArgumentOutOfRangeException>(() => _testSubject.GetTriangleByPosition('A', 1));
+        }
+        
+        [Fact]
+        public void TestGetTriangleCoordinatesResponseMatchesTriangleReturned()
+        {
+            _repo.Setup(_ => _.FindTriangleByRowAndColumn(It.IsAny<char>(), It.IsAny<int>())).Returns(
+                new Triangle(
+                    new Vertex(1, 4),
+                    new Vertex(2, 5),
+                    new Vertex(3, 6),
+                    1,
+                    'A'
+                )
+            );
+            var result = _testSubject.GetTriangleByPosition('A', 1);
+            Assert.Equal(1, result.Vertices[0].X);
+            Assert.Equal(2, result.Vertices[1].X);
+            Assert.Equal(3, result.Vertices[2].X);
+            Assert.Equal(4, result.Vertices[0].Y);
+            Assert.Equal(5, result.Vertices[1].Y);
+            Assert.Equal(6, result.Vertices[2].Y);
+        }
+        
+        [Fact]
+        public void TestGetTriangleCoordinatesRequestsByColumnAndRowToRepo()
+        {
+            _repo.Setup(_ => _.FindTriangleByRowAndColumn(It.IsAny<char>(), It.IsAny<int>())).Returns(
+                new Triangle(
+                    new Vertex(1, 1),
+                    new Vertex(1, 1),
+                    new Vertex(1, 1),
+                    1,
+                    'A'
+                )
+            );
+            _testSubject.GetTriangleByPosition('A', 1);
+            _repo.Verify(_ => _.FindTriangleByRowAndColumn(It.Is<char>(r => r == 'A'), It.Is<int>(c => c == 1)), Times.AtLeastOnce);
         }
         
     }
