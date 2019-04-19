38e46c4dfca73775e21c97e2ce28c0154ce10093
Iain Mackay <me@neirfeno.com>
2019-04-19 09:40:15 +0100

    [add] implementation to return triangle by vertices

--- CalculatedTriangleRepo/TriangleRepo.cs
+++ CalculatedTriangleRepo/TriangleRepo.cs
@@ -1,9 +1,11 @@
+using System.Linq;
 using Triangles.Model;
 using Triangles.TriangleByPosition;
+using Triangles.TriangleByVertices;
 
 namespace CalculatedTriangleRepo
 {
-    public class TriangleRepo: ITriangleByPositionRepo
+    public class TriangleRepo: ITriangleByPositionRepo, ITiangeByVerticesRepo
     {
         private readonly int _triangleSideLength;
         private readonly int _numberOfColumns;
@@ -41,6 +43,54 @@ namespace CalculatedTriangleRepo
             );
         }
 
+        public Triangle FindTriangleByVertices(int v1X, int v1Y, int v2X, int v2Y, int v3X, int v3Y)
+        {
+            if (!(IsValidVertex(v1X, v1Y) && IsValidVertex(v2X, v2Y) && IsValidVertex(v3X, v3Y)))
+            {
+                return null;
+            }
+
+            var vertices = new[] {new Vertex(v1X, v1Y), new Vertex(v2X, v2Y), new Vertex(v3X, v3Y)};
+            var topLeftVertex = FindTopLeftVertex(vertices);
+            var topRightVertex = FindTopRightVertex(vertices, topLeftVertex);
+            var bottomRightVertex = FindBottomRightVertex(vertices, topLeftVertex);
+            var bottomLeftVertex = FindBottomLeftVertex(vertices, topLeftVertex);
+
+            if (!AllVerticesPresent(topRightVertex, bottomLeftVertex, bottomRightVertex))
+            {
+                return null;
+            }
+            
+            var row = (char) (65 + topLeftVertex.Y / 10);
+            var column = topLeftVertex.X / 5 + 1 + (topRightVertex != null ? 1 : 0);
+            return new Triangle(vertices[0], vertices[1], vertices[2], column, row);
+        }
+
+        private bool AllVerticesPresent(Vertex topRightVertex, Vertex bottomLeftVertex, Vertex bottomRightVertex)
+        {
+            return bottomRightVertex != null && (topRightVertex != null || bottomLeftVertex != null);
+        }
+
+        private static Vertex FindBottomLeftVertex(Vertex[] vertices, Vertex topLeftVertex)
+        {
+            return vertices.FirstOrDefault(v => v.X == topLeftVertex.X && v.Y == topLeftVertex.Y + 10);
+        }
+
+        private static Vertex FindTopRightVertex(Vertex[] vertices, Vertex topLeftVertex)
+        {
+            return vertices.FirstOrDefault(v => v.X == topLeftVertex.X + 10 && v.Y == topLeftVertex.Y);
+        }
+
+        private static Vertex FindBottomRightVertex(Vertex[] vertices, Vertex topLeftVertex)
+        {
+            return vertices.FirstOrDefault(v => v.X == topLeftVertex.X + 10 && v.Y == topLeftVertex.Y + 10);
+        }
+
+        private static Vertex FindTopLeftVertex(Vertex[] vertices)
+        {
+            return vertices.OrderBy(v => v.X * 60 + v.Y).First();
+        }
+
         private bool IsValidLocation(int row, int column)
         {
             return column > 0 && row > 0 && column <= _numberOfColumns && row <= _numberOfRows;
@@ -69,5 +119,12 @@ namespace CalculatedTriangleRepo
         {
             return row - 64;
         }
+
+        private static bool IsValidVertex(int x, int y)
+        {
+            return x % 10 == 0 && y % 10 == 0 && x >= 0 && x <= 60 && y >= 0 && y <= 60;
+        }
+        
+        
     }
 }
--- CalculatedTriangleRepoTest/TriangleRepoTest.cs
+++ CalculatedTriangleRepoTest/TriangleRepoTest.cs
@@ -48,5 +48,38 @@ namespace CalculatedTriangleRepoTest
         {
             Assert.Null(_testSubject.FindTriangleByRowAndColumn(row, column));
         }
+        
+        [Theory]
+        [InlineData(1, 'A', 0, 0, 0, 10, 10, 10)]
+        [InlineData(2, 'A', 0, 0, 10, 0, 10, 10)]
+        [InlineData(3, 'A', 10, 0, 10, 10, 20, 10)]
+        [InlineData(4, 'A', 10, 0, 20, 0, 20, 10)]
+        [InlineData(5, 'A', 20, 0, 20, 10, 30, 10)]
+        [InlineData(6, 'A', 20, 0, 30, 0, 30, 10)]
+        [InlineData(7, 'A', 30, 0, 30, 10, 40, 10)]
+        [InlineData(8, 'A', 30, 0, 40, 0, 40, 10)]
+        [InlineData(9, 'A', 40, 0, 40, 10, 50, 10)]
+        [InlineData(10, 'A', 40, 0, 50, 0, 50, 10)]
+        [InlineData(11, 'A', 50, 0, 50, 10, 60, 10)]
+        [InlineData(12, 'A', 50, 0, 60, 00, 60, 10)]
+        [InlineData(1, 'B', 0, 10, 0, 20, 10, 20)]
+        [InlineData(12, 'B', 50, 10, 60, 10, 60, 20)]
+        [InlineData(1, 'F', 0, 50, 0, 60, 10, 60)]
+        [InlineData(12, 'F', 50, 50, 60, 50, 60, 60)]
+        public void TestFindTrianglesByVertices(int column, char row, int v1X, int v1Y, int v2X, int v2Y, int v3X, int v3Y)
+        {
+            var expected = new Triangle(new Vertex(v1X, v1Y), new Vertex(v2X, v2Y), new Vertex(v3X, v3Y), column, row);
+            var result = _testSubject.FindTriangleByVertices(v1X, v1Y, v2X, v2Y, v3X, v3Y);
+            Assert.NotNull(result);
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2, 3, 4, 5)]
+        public void TestOutOfBoundsReturnsNullForFindByVertices(int v1Y, int v1X, int v2Y, int v2X, int v3Y, int v3X)
+        {
+            Assert.Null(_testSubject.FindTriangleByVertices(v1X, v1Y, v2X, v2Y, v3X, v3Y));
+        }
     }
 }
