afa7739c1ca95b964ce857be41b327f377a85533
Iain Mackay <me@neirfeno.com>
2019-04-19 10:57:32 +0100

    [update] to set port via environment variable

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebApp
@@ -12,6 +13,14 @@ namespace WebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel((context, options) =>
+                {
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    if (!string.IsNullOrEmpty(port))
+                    {
+                        options.ListenAnyIP(int.Parse(port));
+                    }
+                });
     }
 }
